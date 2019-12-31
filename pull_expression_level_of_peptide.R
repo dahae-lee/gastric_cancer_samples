e47d82182ccc3be7de0fedb7eae69c207af31c11
dahae-lee101 <googoois99@naver.com>
2019-12-31 20:29:14 +0900

    update

--- pull_expression_level_of_peptide.R
+++ pull_expression_level_of_peptide.R
@@ -2,7 +2,7 @@ options(stringsAsFactors = F)
 args = commandArgs(trailingOnly=TRUE)
 library(readxl)
 
-#f = '~/GoogleDrive/EOGC_Nitration/summary_files/N155T156_Nitration_in_Global.xlsx'
+# f = '~/GoogleDrive/EOGC_Nitration/summary_files_201912/N5357T5374_Global_nitration.xlsx'
 f = args[1]
 sample = strsplit(strsplit(f, 'summary_files_201912/', fixed = T)[[1]][2], '_')[[1]][1]
 
@@ -29,5 +29,30 @@ e = e[!(e$norm1==0 | e$norm2==0 | e$tumor1==0 | e$tumor2==0),]
 # select the expression levels only and add the sample number in new column
 e <- e[,5:8]
 e$sample_id <- sample
-e_out = paste('/Users/dahaelee/GoogleDrive/gastric_cancer_samples/Tables/peptide_expression/table.peptide_expression', sample, 'txt', sep='.')
-write.table(e, e_out, sep='\t', quote = F, row.names = F, col.names = T)
+
+#change data frame into the tidy form in order to make plot
+library(tidyverse)
+n1 <- e[,c(1,5)] %>% mutate(celltype = 'norm1') %>% rename('expression' = 'norm1')
+t1 <- e[,c(2,5)] %>% mutate(celltype = 'tumor1') %>% rename('expression' = 'tumor1')
+n2 <- e[,c(3,5)] %>% mutate(celltype = 'norm2') %>% rename('expression' = 'norm2')
+t2 <- e[,c(4,5)] %>% mutate(celltype = 'tumor2') %>% rename('expression' = 'tumor2')
+a <- rbind.data.frame(n1, t1, n2, t2)
+
+#draw plot
+
+pa <- a %>% 
+  ggplot(aes(x = celltype, y = expression, fill = celltype))+
+  geom_boxplot(position=position_dodge(1))+
+  scale_fill_manual(breaks = c("norm1", "norm2", "tumor1", "tumor2"), 
+                    values=c("grey", "grey", "firebrick", "firebrick"))+
+  xlab("Cell Type")+
+  ylab("Peptide epxpression")+
+  ggtitle(sample)+
+  theme(plot.title = element_text(hjust = 0.5, face = "bold"))
+
+pa
+
+#save plot
+pa_out <- paste('~/GoogleDrive/gastric_cancer_samples/Figures/peptide_expression/
+                plot_peptide_expression_level', sample, 'pdf', sep = ".")
+ggsave(pa_out, pa)
